eae1a21f4b99a976b04729f26d7f98eb05323ab4
Valmir silva <valmirdasilva1999@gmail.com>
2021-03-10 10:08:43 -0300

    objeto

---
+++ fundamentos/objeto.js
@@ -0,0 +1,12 @@
+const objeto = {}
+objeto.nome = 'joão'
+objeto.idade = '23'
+objeto['atribuindo'] = 52.11 //Não recomendado este tipo de atribuição
+console.log(objeto)
+
+const objeto2 = {
+    nome: 'Junior',
+    cidade: 'Sapiranga',
+    number: 998309401
+}
+console.log(objeto2)
