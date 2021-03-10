8eeb391dc16d1e0515af41fa5f2fe2d30244a9b8
Valmir silva <valmirdasilva1999@gmail.com>
2021-03-10 17:47:33 -0300

    variaveisEConstantes | nullUndefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,11 @@
+const a = null
+console.log(typeof null) // é validado como um objeto e não null
+
+produto.preco = undefined // evitar atribuir undefined
+
+// delete produto.preco | MODO CORRETO DE TIRAR O ATRIBUTO DE UMA TABELA
+
+
+
+var a = undefined
+console.log(a)
--- fundamentos/variaveisEConstantes.js
+++ fundamentos/variaveisEConstantes.js
@@ -1,3 +1,8 @@
 var a = 3
 let b = 4
 
+console.log('maria,pedro,zezinho'.split(','))
+
+let escola = "cod3r"
+console.log(`Escola ${escola}!`)
+console.log(escola.charCodeAt(3))
